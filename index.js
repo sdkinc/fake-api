9b5df36a1f946d4e4c64a52a1927242c61e677c8
Oleksandr Zhurba <starkyzh@gmail.com>
2023-07-19 07:34:13 +0200

    feat: add solution

---
+++ index.js
@@ -0,0 +1,33 @@
+const tableElement = document.getElementById("table-element");
+const headElement = document.getElementById("header");
+
+async function getCheck() {
+    const response = await fetch("https://sdkinc.github.io/fake-api/checks_export.json");
+    const check = await response.json();
+    const { Чеки } = check;
+    Чеки.forEach(element => {
+        const p = document.createElement("p");
+        const { Номер, Дата, СуммаВКассу, Товары } = element;
+        p.textContent = "Чек №" + Номер + " от " + Дата + " на сумму " + СуммаВКассу;
+        headElement.append(p);
+        Товары.forEach(element => {
+            const { Наименование, Количество, ЦенаСНДС, СуммаСНДС } = element;
+            const tr = document.createElement("tr");
+            let td = document.createElement("td");
+            td.textContent = Наименование;
+            tr.append(td);
+            td = document.createElement("td");
+            td.textContent = Количество;
+            tr.append(td);
+            td = document.createElement("td");
+            td.textContent = ЦенаСНДС;
+            tr.append(td);
+            td = document.createElement("td");
+            td.textContent = СуммаСНДС;
+            tr.append(td);
+            tableElement.append(tr);
+        });
+    });
+}
+
+getCheck();
